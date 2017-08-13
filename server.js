f33d43ab5525fad37fdfb13932647dc946364147
Kaushik M K <mkkaushiksjce@gmail.com>
2017-08-13 20:48:56 +0530

    production issue fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const compress = require('compression');
 const auth = require('http-auth');
 const path = require('path');
 const compression = require('compression');
-const config = require('./config/config.js');
+const config = require('./config/server_config.js');
 
 //to throw uncaught exception error
 process.on('uncaughtException',  (exception) => {
