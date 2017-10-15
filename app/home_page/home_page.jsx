3c4eaaf446418778201cd4c57a62023c5b426aba
Kaushik M K <mkkaushiksjce@gmail.com>
2017-10-15 11:57:47 +0530

    home page and nav page added

--- app/home_page/home_page.jsx
+++ app/home_page/home_page.jsx
@@ -7,19 +7,24 @@ class HomePage extends React.Component{
     super(props);
   }
 
-  componentDidMount = ()=>{
-    console.log('heello');
-  }
 
   render(){
     const {props, state} = this;
 
     return(
-      <div className="landing">Mayaskara
-      </div>
-    )
-  }
-}
+          <div className="homepage">
+            <div className="home-section-wrap">
+              <div className="overlay"></div>
+              <div className="title-wrap">
+                <h1 className="title">MATERIAL DESIGN AGENCY WP THEME</h1>
+                <div className="subtitle">SUPER CLEAN MINIMAL DESIGN. AWESOME FEATURES WITH ENDLESS POSSIBILITIES.</div>
+                <div id="contactBtn" className="button">GET IN TOUCH</div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    }
 
 HomePage.propTypes = {
 };
